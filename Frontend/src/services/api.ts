82be31a3f751907b26af33af344dc1b0786e15d9
allmight777 <agoliganange15@gmail.com>
2026-03-05 08:48:45 +0100

    Fix all localhost:8000 -> VITE_API_URL for production

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -168,7 +168,7 @@ export interface PaginatedResponse<T> {
 }
 
 // 🔹 baseURL = http://localhost:8000/api
-const API_URL = 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
