3a684b707c325183d1e3f07c4d6f8aab19e23726
allmight777 <agoliganange15@gmail.com>
2026-03-05 13:51:15 +0100

    Add auth.login view - redirect to frontend

---
+++ Backend/resources/views/auth/login.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"><title>Redirection...</title></head>
+<body>
+<script>
+    window.location.href = 'https://gestiloc-frontend.vercel.app/login';
+</script>
+</body>
+</html>
